be74cd5b5c8543241d690f7852b1404404b06170
eddie chen <eddiechen@eddiede-MacBook-Pro.local>
2019-03-13 10:34:32 +0800

    add number = 3

--- git test1/ViewController.swift
+++ git test1/ViewController.swift
@@ -11,8 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
+          let number = 3
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+      
+        print(number)
     }
 
 
